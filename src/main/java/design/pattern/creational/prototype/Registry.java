12e69e2056641bf452c0d1b55529ebc73ff80667
Raj Tomar <rajtomar50@gmail.com>
2021-03-04 16:41:08 +0530

    Prototype Design Pattern added

---
+++ src/main/java/design/pattern/creational/prototype/Registry.java
@@ -0,0 +1,37 @@
+package design.pattern.creational.prototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Registry {
+
+	private Map<String, Item> items = new HashMap<>();
+
+	public Registry() {
+		loadItems();
+	}
+
+	public Item createItem(String type) {
+		Item item = null;
+		try {
+			item = (Item)(items.get(type)).clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return item;
+	}
+
+	private void loadItems() {
+		Movie movie = new Movie();
+		movie.setTitle("Basic Movie");
+		movie.setPrice(23.99);
+		movie.setRuntime("2 hours");
+		items.put("Movie", movie);
+		
+		Book book = new Book();
+		book.setTitle("Basic Book");
+		book.setPrice(23.99);
+		book.setNumberOfPages(220);
+		items.put("Book", book);
+	}
+}
